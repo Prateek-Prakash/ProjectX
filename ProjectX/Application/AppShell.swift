d89b4a235003a577667def47e90d5d87d5ac8620
Prateek-Prakash <63609044+Prateek-Prakash@users.noreply.github.com>
2026-01-22 19:04:31 -0500

    Fix Build Issue

--- ProjectX/Application/AppShell.swift
+++ ProjectX/Application/AppShell.swift
@@ -46,7 +46,6 @@ struct AppShell: View {
                                     }
                                 }
                             }
-                            renderedImage
                         }
                         .padding(.horizontal)
                         .padding(.bottom)
